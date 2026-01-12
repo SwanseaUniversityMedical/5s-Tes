2c2451591231298a90013feb4c25e5a2e65e09c4
AndrewThien <anretrithien@gmail.com>
2026-01-12 14:12:22 +0000

    improve the logic of sign-in page

--- Agent/agent-web/app/sign-in/page.tsx
+++ Agent/agent-web/app/sign-in/page.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import { signIn } from "@/lib/auth-client";
+import { signIn, useSession } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
+// A page purely to navigate a user to Keycloak log in page should Better Auth ask to log in.
 export default function Signin() {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   useEffect(() => {
-    void signIn.oauth2({
-      providerId: "keycloak",
-      callbackURL: window.location.origin + "/",
-    });
-  }, []);
+    if (!session?.user) {
+      void signIn.oauth2({
+        providerId: "keycloak",
+        callbackURL: window.location.origin + "/",
+      });
+    } else if (session?.user) {
+      void router.push("/");
+    }
+  }, [session, router]);
 
   return <div></div>;
 }
