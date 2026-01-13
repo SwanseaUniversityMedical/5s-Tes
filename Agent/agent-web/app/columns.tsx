798f32ee75a9cfe33e5bf01eaaef350e3f5f274b
AndrewThien <anretrithien@gmail.com>
2026-01-13 17:05:51 +0000

    improve columns

--- Agent/agent-web/app/columns.tsx
+++ Agent/agent-web/app/columns.tsx
@@ -3,6 +3,8 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { TreProject } from "@/types/TreProject";
 import { getDecisionInfo } from "@/types/Decision";
+import { format } from "date-fns/format";
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<TreProject>[] = [
   {
@@ -25,7 +27,11 @@ export const columns: ColumnDef<TreProject>[] = [
     accessorKey: "lastDecisionDate",
     header: "Last Decision Date",
     cell: ({ row }) => {
-      return <div>{row.original.lastDecisionDate}</div>;
+      return (
+        <div>
+          {format(new Date(row.original.lastDecisionDate), "d MMM yyyy HH:mm")}
+        </div>
+      );
     },
   },
   {
@@ -33,6 +39,23 @@ export const columns: ColumnDef<TreProject>[] = [
     header: "Approved By",
     cell: ({ row }) => {
       return <div>{row.original.approvedBy}</div>;
+    },
+  },
+  {
+    header: "Actions",
+    cell: ({ row }) => {
+      // TODO: Add review logic
+      return (
+        <div>
+          <Button
+            variant="secondary"
+            className="hover:bg-secondary/80 cursor-pointer"
+            size="sm"
+          >
+            Review
+          </Button>
+        </div>
+      );
     },
   },
 ];
