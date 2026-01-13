387ed4db8a2b4b673a727d111bcc188c451a9986
AndrewThien <anretrithien@gmail.com>
2026-01-13 15:17:47 +0000

    add a common request function

---
+++ Agent/agent-web/lib/api/request.ts
@@ -0,0 +1,68 @@
+import { auth } from "../auth";
+import { headers } from "next/headers";
+
+interface RequestOptions {
+  method?: string;
+  headers?: Record<string, string>;
+  body?: BodyInit;
+  download?: boolean;
+  cache?: RequestCache;
+  next?: { revalidate: number };
+  authMode?: "token" | "apiKey";
+  baseUrl?: string;
+}
+
+const request = async <T>(url: string, options: RequestOptions = {}) => {
+  // Set Default to requiring auth for backward compatibility (Django)
+
+  const baseUrl = options.baseUrl ?? `${process.env.AGENT_API_URL}/api`;
+
+  let requestHeaders: Record<string, string> = { ...(options.headers || {}) };
+
+  const accessToken = await auth.api.getAccessToken({
+    body: {
+      providerId: "keycloak",
+    },
+    headers: await headers(),
+  });
+  if (accessToken.accessToken) {
+    requestHeaders.Authorization = `Bearer ${accessToken.accessToken}`;
+  }
+
+  const response = await fetch(`${baseUrl}/${url}`, {
+    method: options.method || "GET",
+    headers: requestHeaders,
+    body: options.body,
+    cache: options.cache,
+    next: options.next,
+  });
+  const contentType = response.headers.get("Content-Type");
+  // TODO: add error handling for the response
+  if (!response.ok) {
+    let errorMessage = "An error occurred";
+    if (contentType && contentType.includes("application/json")) {
+      try {
+        const errorResponse = await response.json();
+        if (Array.isArray(errorResponse)) {
+          errorMessage = errorResponse.join(" * ");
+        } else {
+          errorMessage = errorResponse.detail || errorMessage;
+        }
+      } catch (error) {
+        errorMessage = "Failed to parse error response";
+      }
+    }
+    throw new Error(errorMessage);
+  }
+
+  if (response.status === 204) {
+    return {} as T;
+  }
+
+  if (contentType && contentType.includes("application/json")) {
+    return response.json();
+  }
+  return response.text();
+};
+
+export default request;
