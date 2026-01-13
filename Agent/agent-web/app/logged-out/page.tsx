244b11241c6e3dcde339ec04dbe3701f0857fd98
AndrewThien <anretrithien@gmail.com>
2026-01-13 17:37:26 +0000

    fix workflow and build containers

--- Agent/agent-web/app/logged-out/page.tsx
+++ Agent/agent-web/app/logged-out/page.tsx
@@ -1,10 +1,6 @@
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
 import { AuthButton } from "@/components/auth-button";
 
 export default async function Home() {
-  const session = await auth.api.getSession({ headers: await headers() });
-
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-2xl font-bold">Agent Web UI Application</h1>
