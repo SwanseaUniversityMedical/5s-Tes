2702d69a7a681144037f178a302043498e258385
CodeByKarthik <karthinottsuk@gmail.com>
2026-01-14 10:11:16 +0000

    Added Routes for Menubar Icons

---
+++ Agent/agent-web/app/access-rules/page.tsx
@@ -0,0 +1,7 @@
+export default function AccessRules() {
+    return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
+      Page for Access Rules
+    </div>
+  );
+}
