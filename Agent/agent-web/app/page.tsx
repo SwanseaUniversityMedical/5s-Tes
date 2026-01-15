393d83fa5bfb2e8d17ef20c5bfd38cae2e5ba016
CodeByKarthik <karthinottsuk@gmail.com>
2026-01-15 17:04:54 +0000

    Resolved Conflict

--- Agent/agent-web/app/page.tsx
+++ Agent/agent-web/app/page.tsx
@@ -7,11 +7,6 @@ export const metadata: Metadata = {
   description: "Agent Web UI Application",
 };
 
-  return (
-    <div className="font-sans grid items-center justify-items-center pt-24 p-8">
-      <h1 className="text-2xl font-bold">Agent Web UI Application</h1>
-    </div>
-  );
 export default async function Home() {
   // check if user is authenticated and has the required role and then redirect to projects page
   await authcheck("dare-tre-admin");
