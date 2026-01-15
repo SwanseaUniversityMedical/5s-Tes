0eb0a0635891efbc56944c989817ad300b3ef79e
AndrewThien <anretrithien@gmail.com>
2026-01-15 12:25:58 +0000

    improve logout page

--- Agent/agent-web/app/logged-out/page.tsx
+++ Agent/agent-web/app/logged-out/page.tsx
@@ -2,9 +2,9 @@ import { AuthButton } from "@/components/auth-button";
 
 export default async function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans flex flex-col items-center justify-center min-h-screen p-8 pb-20">
       <h1 className="text-2xl font-bold">Agent Web UI Application</h1>
-      You are logged out!
+      <p className="my-3 text-gray-500 mt-2">You are logged out!</p>
       <AuthButton mode="login" />
     </div>
   );
