33f9ada28fb981f01e4a35c7e66d20bc8bb70d28
CodeByKarthik <karthinottsuk@gmail.com>
2026-01-16 13:33:43 +0000

    Added Title and Description for Updating Credentials Page

--- Agent/agent-web/app/configure-5s-tes/page.tsx
+++ Agent/agent-web/app/configure-5s-tes/page.tsx
@@ -1,8 +1,18 @@
 import CredentialsTabs from "@/components/credentials/CredentialsTab";
 
 export default function UpdateCredentials() {
-    return (
-    <div className="flex justify-start p-40 pt-10">
+  return (
+
+    <div className="px-20 pt-6 pl-36">
+      {/* Page Header */}
+      <div className="mb-6">
+        <h1 className="text-2xl font-bold">Configure 5S-TES</h1>
+        <p className="mt-2 text-gray-600">
+          Configure 5S-TES TRE Admin with credentials to access Submission/Egress apps.
+        </p>
+      </div>
+
+      {/* Credentials Tabs */}
       <CredentialsTabs />
     </div>
   );
