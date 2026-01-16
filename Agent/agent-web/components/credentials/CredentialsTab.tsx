5bc2d1845121f1dcc91a2b848b13af18c73a7388
CodeByKarthik <karthinottsuk@gmail.com>
2026-01-16 16:31:17 +0000

    Revoke Tabs Styling Commit

--- Agent/agent-web/components/credentials/CredentialsTab.tsx
+++ Agent/agent-web/components/credentials/CredentialsTab.tsx
@@ -3,24 +3,21 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import CredentialsForm from "./CredentialsForm";
 
-// Define default styling for Tabs Components
-const TabsDefaultStyling =
-  "data-[state=active]:bg-gray-900 data-[state=active]:text-white";
-
 // Creates Credentials Tabs with Submission and Egress sections
+
 export default function CredentialsTabs() {
   return (
     <Tabs
       defaultValue="submission"
       className="w-90"
     >
-      {/* Tabs List + Help Tooltip */}
+      {/* Tabs List */}
       <div className="flex items-center gap-2 border-b border-black pb-2 -mx-4 px-4">
-        <TabsList className="bg-transparent border border-gray-300">
-          <TabsTrigger value="submission" className={TabsDefaultStyling}>
+        <TabsList>
+          <TabsTrigger value="submission">
             Submission
           </TabsTrigger>
-          <TabsTrigger value="egress" className={TabsDefaultStyling}>
+          <TabsTrigger value="egress">
             Egress
           </TabsTrigger>
         </TabsList>
