7826c391ef59b7f69915547a5b2ec63673ef12fb
AndrewThien <anretrithien@gmail.com>
2026-01-16 11:26:19 +0000

    start getting one project

--- Agent/agent-web/lib/api/projects.ts
+++ Agent/agent-web/lib/api/projects.ts
@@ -4,10 +4,16 @@ import { TreProject } from "@/types/TreProject";
 const fetchKeys = {
   listProjects: (params: { showOnlyUnprocessed: boolean }) =>
     `Approval/GetAllTreProjects?showOnlyUnprocessed=${params.showOnlyUnprocessed}`,
+  getProject: (projectId: string) =>
+    `Approval/GetTreProject?projectId=${projectId}`,
 };
 
 export async function getProjects(params: {
   showOnlyUnprocessed: boolean;
 }): Promise<TreProject[]> {
   return await request<TreProject[]>(fetchKeys.listProjects(params));
+}
+
+export async function getProject(projectId: string): Promise<TreProject> {
+  return await request<TreProject>(fetchKeys.getProject(projectId));
 }
