5b2dd6317fea030ef74f283e1d45dc8c18fde5ff
CodeByKarthik <karthinottsuk@gmail.com>
2026-01-30 19:17:20 +0000

    Added Types

--- Agent/agent-web/types/access-rules.ts
+++ Agent/agent-web/types/access-rules.ts
@@ -9,109 +9,27 @@ export type RuleColumns = {
 
 export type RuleAction = "edit" | "delete" | "save";
 
-export const DUMMY_RULES: RuleColumns[] = [
-  {
-    description: "-",
-    inputUser: "Any",
-    inputProject: "Any",
-    outputTag: '"trinoUsername"',
-    outputValue: 'string(project) + "-user-" + string(user)',
-    outputEnv: '"trino"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"submissionId"',
-    outputValue: "string(submissionId)",
-    outputEnv: '"trino"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"trinoPassword"',
-    outputValue: '""',
-    outputEnv: '"trino"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"trinoURL"',
-    outputValue: '"http://localhost"',
-    outputEnv: '"trino"',
-  },
-  {
-    description: "-",
-    inputUser: "Any",
-    inputProject: "Any",
-    outputTag: '"postgresUsername"',
-    outputValue: 'string(project) + "-user-" + string(user)',
-    outputEnv: '"postgres"',
-  },
-  {
-    description: "-",
-    inputUser: "Any",
-    inputProject: "Any",
-    outputTag: '"postgresPassword"',
-    outputValue: '""',
-    outputEnv: '"postgres"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"postgresDatabase"',
-    outputValue: '"tredata"',
-    outputEnv: '"postgres"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"postgresServer"',
-    outputValue: '"localhost"',
-    outputEnv: '"postgres"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"postgresPort"',
-    outputValue: '"32769"',
-    outputEnv: '"postgres"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"postgresSchema"',
-    outputValue: "string(project)",
-    outputEnv: '"postgres"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"submissionId"',
-    outputValue: "string(submissionId)",
-    outputEnv: '"postgres"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"myapikey"',
-    outputValue: '"123"',
-    outputEnv: '"tre"',
-  },
-  {
-    description: "-",
-    inputUser: "Empty",
-    inputProject: "Empty",
-    outputTag: '"submissionId"',
-    outputValue: "string(submissionId)",
-    outputEnv: '"tre"',
-  },
-];
+
+interface DmnRuleSubData {
+  id?: string | null;
+  text?: string | null;
+}
+
+export interface DmnRule {
+  id?: string | null;
+  description?: string | null;
+  inputEntries?: Array<DmnRuleSubData> | null;
+  outputEntries?: Array<DmnRuleSubData> | null;
+}
+
+
+export interface DecisionInfo {
+  decisionId: string;
+  decisionName: string;
+  hitPolicy: string;
+}
+
+export interface AccessRulesData {
+  rules: RuleColumns[];
+  info: DecisionInfo;
+}
