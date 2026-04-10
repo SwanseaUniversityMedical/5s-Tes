84ffb3567ef33df79720fd776a8bace3b6777bd5
AndrewThien <anretrithien@gmail.com>
2026-04-10 17:57:31 +0100

    Revert "clean up"
    This reverts commit 7922b29604bcf60bba26d514a737bc792e51da7e.

---
+++ Shared/FiveSafesTes.Core/Models/ViewModels/DashboardCounts.cs
@@ -0,0 +1,12 @@
+namespace FiveSafesTes.Core.Models.ViewModels
+{
+    public class DashboardCounts
+    {
+        public int ProjectCount { get; set; }
+        public int SubmissionCount { get; set; }
+        public int UserCount { get; set; }
+        public int TreCount { get; set; }
+        public int UserOnProjectCount { get; set; }
+        public int UserWroteSubmissionCount { get; set; }
+    }
+}
