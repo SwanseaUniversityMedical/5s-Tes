f45e96794f94a6cfec6d7f368a8b979089f8dbd3
AndrewThien <anretrithien@gmail.com>
2026-04-10 18:34:31 +0100

    revert some more

--- Submission/Submission.Api/Controllers/ProjectController.cs
+++ Submission/Submission.Api/Controllers/ProjectController.cs
@@ -577,19 +577,7 @@ namespace Submission.Api.Controllers
         {
             try
             {
-                List<Tre> tres = _DbContext.Projects
-                    .AsNoTracking()
-                    .Where(p => p.Id == projectId)
-                    .SelectMany(p => p.Tres)
-                    .Select(t => new Tre
-                    {
-                        Id = t.Id,
-                        Name = t.Name,
-                        LastHeartBeatReceived = t.LastHeartBeatReceived,
-                        About = t.About,
-                        FormData = t.FormData
-                    })
-                    .ToList();
+                List<Tre> tres = _DbContext.Projects.Where(p => p.Id == projectId).SelectMany(p => p.Tres).ToList();
 
                 return tres;
             }
@@ -724,7 +712,6 @@ namespace Submission.Api.Controllers
                 //    c.Users.Any(t => t.Name.ToLower().Contains(searchString.Trim().ToLower())) ||
                 //    c.Tres.Any(t => t.Name.ToLower().Contains(searchString.Trim().ToLower())) || c.Submissions.Any(s => s.TesName.Contains(searchString.Trim().ToLower()))).ToList();
                 string normalizedSearchString = $"%{searchString.Trim()}%";
-                
                 List<Project> searchResults = _DbContext.Projects
 
                     .Include(c => c.Users)
@@ -732,15 +719,14 @@ namespace Submission.Api.Controllers
                     .Include(c => c.Submissions)
 
                     .Include(c => c.Tres)
-                    
                     .Where(c => EF.Functions.Like(c.Name, normalizedSearchString) ||
 
 
-                                  c.Users.Any(t => EF.Functions.Like(t.Name, normalizedSearchString)) ||
+                                c.Users.Any(t => EF.Functions.Like(t.Name, normalizedSearchString)) ||
 
-                                  c.Tres.Any(t => EF.Functions.Like(t.Name, normalizedSearchString)) ||
+                                c.Tres.Any(t => EF.Functions.Like(t.Name, normalizedSearchString)) ||
 
-                                  c.Submissions.Any(s => EF.Functions.Like(s.TesName, normalizedSearchString))
+                                c.Submissions.Any(s => EF.Functions.Like(s.TesName, normalizedSearchString))
 
                     )
 
