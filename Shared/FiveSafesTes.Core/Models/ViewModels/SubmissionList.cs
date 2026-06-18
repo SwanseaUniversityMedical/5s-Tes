ea12a5ecbd605cb18ed7d277ddf90b113b2dbbfa
AndrewThien <anretrithien@gmail.com>
2026-06-18 12:20:18 +0100

    improve submission list

--- Shared/FiveSafesTes.Core/Models/ViewModels/SubmissionList.cs
+++ Shared/FiveSafesTes.Core/Models/ViewModels/SubmissionList.cs
@@ -9,7 +9,7 @@ namespace FiveSafesTes.Core.Models.ViewModels
     public class SubmissionList
     {
         public List<Project.ProjectSubmissionDto> SubmissionsDTO{ get; set; }
-        public List<Submission> Submissions { get; set; }
+        public List<Submission.SubmissionSummary> Submissions { get; set; }
         public bool UseParent { get; set; }
     }
 }
