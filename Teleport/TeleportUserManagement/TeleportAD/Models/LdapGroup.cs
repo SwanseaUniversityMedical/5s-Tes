a770737e514ca99e24cc357b387b048ab3d015e5
Sam <72132397+itismarshs@users.noreply.github.com>
2026-07-22 12:05:08 +0100

    Trim ldap models

--- Teleport/TeleportUserManagement/TeleportAD/Models/LdapGroup.cs
+++ Teleport/TeleportUserManagement/TeleportAD/Models/LdapGroup.cs
@@ -5,36 +5,10 @@ namespace TeleportUserManagement.Models
     public class LdapGroup
     {
         public string DistinguishedName { get; }
-        public string CommonName { get; }
-        public string Description { get; }
-
-        public LdapEntry RawEntry { get; set; }
-        public List<string> Members { get; }
-
-        public Dictionary<string, string[]> Properties { get; } = new();
 
         public LdapGroup(LdapEntry entry)
         {
             DistinguishedName = entry.Dn;
-            var atts = entry.GetAttributeSet();
-            CommonName = GetAttributeSafe(atts, "cn");
-            Description = GetAttributeSafe(atts, "description");
-            Members = GetAttributeArraySafe(atts, "member");
-            RawEntry = entry;
-            foreach (var attr in atts)
-            {
-                Properties[attr.Name] = attr.StringValueArray;
-            }
-        }
-
-        private static string GetAttributeSafe(LdapAttributeSet attrs, string name)
-        {
-            return attrs.TryGetValue(name, out var attr) ? attr?.StringValue : null;
-        }
-
-        private static List<string> GetAttributeArraySafe(LdapAttributeSet attrs, string name)
-        {
-            return attrs.TryGetValue(name, out var attr) ? attr?.StringValueArray?.ToList() : [];
         }
     }
 }
--- Teleport/TeleportUserManagement/TeleportAD/Models/LdapUser.cs
+++ Teleport/TeleportUserManagement/TeleportAD/Models/LdapUser.cs
@@ -1,5 +1,4 @@
 using System.DirectoryServices.Protocols;
-using System.Security.Principal;
 
 namespace TeleportUserManagement.Models
 {
@@ -9,30 +8,12 @@ namespace TeleportUserManagement.Models
         public string SamAccountName { get; set; }
         public string UserPrincipalName { get; set; }
         public string DisplayName { get; set; }
-        public SearchResultEntry RawEntry { get; set; }
         public string EmailAddress { get; set; }
         public string Description { get; set; }
         public string GivenName { get; set; }
         public string Surname { get; set; }
 
-        public SecurityIdentifier Sid { get; set; }
 
-        public int? UserAccountCtrl { get; set; }
-        public string Name { get; set; }
-        public string TelephoneNumber { get; set; }
-        public List<string> MemberOf { get; set; }
-
-
-        public string StringSid { get; set; }
-
-
-
-
-        public LdapUser(string username, string sid)
-        {
-            SamAccountName = username;
-            StringSid = sid;
-        }
         public LdapUser(SearchResultEntry entry)
         {
             var attrs = entry.Attributes;
@@ -44,54 +25,11 @@ namespace TeleportUserManagement.Models
             GivenName = GetSearchResultAttributeSafe(attrs, "givenName");
             Surname = GetSearchResultAttributeSafe(attrs, "sn");
             Description = GetSearchResultAttributeSafe(attrs, "description");
-            Name = GetSearchResultAttributeSafe(attrs, "name");
-            TelephoneNumber = GetSearchResultAttributeSafe(attrs, "telephoneNumber");
-            MemberOf = GetearchResultAttributeArraySafe(attrs, "memberOf");
-            Sid = GetSearchResultSidSafe(attrs, "objectSid");
-
-            var userAccountControlAttr = GetSearchResultAttributeSafe(attrs, "userAccountControl");
-            if (!string.IsNullOrWhiteSpace(userAccountControlAttr))
-            {
-                UserAccountCtrl = int.Parse(userAccountControlAttr);
-            }
-
-            RawEntry = entry;
-
-
         }
-
-
 
         private static string GetSearchResultAttributeSafe(SearchResultAttributeCollection attrs, string name)
         {
             return attrs.Contains(name) ? attrs[name][0]?.ToString() : "";
         }
-
-        private static SecurityIdentifier GetSearchResultSidSafe(SearchResultAttributeCollection attrs, string name)
-        {
-            if (!attrs.Contains(name) || attrs[name].Count == 0)
-                return null;
-
-            var sidBytes = (byte[])attrs[name][0];
-            return new SecurityIdentifier(sidBytes, 0);
-        }
-
-        private static List<string> GetearchResultAttributeArraySafe(SearchResultAttributeCollection attrs, string name)
-        {
-
-
-            var memberOfList = new List<string>();
-
-            if (!attrs.Contains(name)) return memberOfList;
-            // GetValues(typeof(string)) returns an object[] of all group DNs
-            var values = attrs[name].GetValues(typeof(string));
-
-            // Cast each object to string and convert to List<string>
-            memberOfList = values.Cast<string>().ToList();
-
-            return memberOfList;
-        }
-
-
     }
 }
